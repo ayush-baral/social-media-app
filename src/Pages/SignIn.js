b85bdfdd9baaaa9b45953cfa2a29e9bcda606ddb
Ayush <ayushbaral17@gmail.com>
2023-03-03 00:04:42 +0545

    navigate to sign in page from signup page and vice versa

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -4,8 +4,10 @@ import { auth } from '../firebase';
 import { useForm } from 'react-hook-form';
 import Input from '../Components/UI/Input';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const {
     register,
@@ -38,35 +40,48 @@ const SignIn = () => {
     <div className='flex flex-col justify-center items-center pt-10'>
       <div>
         <h2 className='text-2xl font-bold mb-10'>SIGN IN</h2>
-      </div>
 
-      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-y-4'>
-        <Input
-          label={'Email'}
-          name='email'
-          register={register}
-          errors={errors}
-          rules={{
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'invalid email address',
-            },
-          }}
-        />
-        <Input
-          label={'Password'}
-          name='password'
-          register={register}
-          errors={errors}
-        />
-        <button
-          type='submit'
-          className='py-[10px] px-[18px] rounded-lg disabled:cursor-not-allowed bg-lime-400'
-          disabled={loading}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='flex flex-col gap-y-4'
         >
-          {!loading ? 'SignIn' : 'Signing up...'}
-        </button>
-      </form>
+          <Input
+            label={'Email'}
+            name='email'
+            register={register}
+            errors={errors}
+            rules={{
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'invalid email address',
+              },
+            }}
+          />
+          <Input
+            label={'Password'}
+            name='password'
+            register={register}
+            errors={errors}
+          />
+          <button
+            type='submit'
+            className='py-[10px] px-[18px] rounded-lg disabled:cursor-not-allowed bg-lime-400'
+            disabled={loading}
+          >
+            {!loading ? 'SignIn' : 'Signing up...'}
+          </button>
+        </form>
+
+        <p className='mt-4'>
+          Not a user?{' '}
+          <span
+            className='underline cursor-pointer'
+            onClick={() => navigate('/signup')}
+          >
+            Sign up
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
